cdc2f0b0b82b47e2f69054e865ded372713024da
Jason Patton <jpatton@cs.wisc.edu>
2026-02-27 14:16:56 -0600

    INF-3590 Whoops never changed the query

--- accounting/filters/OsgScheddResourcesFilter.py
+++ accounting/filters/OsgScheddResourcesFilter.py
@@ -108,6 +108,25 @@ DEFAULT_FILTER_ATTRS = [
 ]
 
 
+RESOURCE_TYPES = ["Cpus", "Memory", "Disk", "Gpus"]
+FLOORED_RESOURCE_FIELD = "FlooredRequest{resource}"
+FLOORED_RESOURCE_SCRIPT = """
+if (
+    doc.containsKey("{resource}Provisioned") &&
+    doc["{resource}Provisioned"].size() > 0 &&
+    doc.containsKey("Request{resource}") &&
+    doc["Request{resource}"].size() > 0 &&
+    doc["{resource}Provisioned"].value < doc["Request{resource}"].value
+    ) {{
+        emit(doc["{resource}Provisioned"].value);
+}} else if (
+    doc.containsKey("Request{resource}") &&
+    doc["Request{resource}"].size() > 0) {{
+        emit(doc["Request{resource}"].value);
+}}
+"""
+
+
 # INSTITUTION_DB = get_institution_database()
 # RESOURCE_DATA = get_topology_resource_data()
 
@@ -259,6 +278,19 @@ class OsgScheddResourcesFilter(BaseFilter):
                 }
             }
         }
+        # Add floored resource requests (INF-3590)
+        fields = []
+        runtime_mappings = {}
+        for resource in RESOURCE_TYPES:
+            fields.append(FLOORED_RESOURCE_FIELD.format(resource=resource))
+            runtime_mappings[FLOORED_RESOURCE_FIELD.format(resource=resource)] = {
+                "type": "long",
+                "script": {
+                    "source": FLOORED_RESOURCE_SCRIPT.format(resource=resource)
+                }
+            }
+        query["fields"] = fields
+        query["runtime_mappings"] = runtime_mappings
         return query
 
 
